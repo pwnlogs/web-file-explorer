f9ecbe3c3541014fad5794423bc882249f1d6989
Jithin Pavithran <jithinpavithran96@gmail.com>
2020-08-29 07:05:24 +0530

    first functional commit

--- index.php
+++ index.php
@@ -1,24 +1,73 @@
 <html>
 <head>
     <title>Dir View</title>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+    <style>
+        body{
+            margin: 0;
+            padding: 0;
+        }
+        .container{
+            display: flex;
+            flex-direction: row;
+            height: 100%;
+        }
+        .browse{
+            overflow: scroll;
+            max-width: 20%;
+            flex-grow: 1;
+            flex-shrink: 2;
+        }
+        .preview{
+            overflow: scroll;
+            background: #a0a0a0;
+            width: auto;
+            flex-grow: 2;
+            flex-shrink: 1;
+        }
+        .dir{
+        }
+        .file{
+
+        }
+        .dir-a{
+            background-color: #dbdbdb;
+        }
+        .file-a{
+            background-color: #f0f0f0;
+        }
+        .entry{
+            display: block;
+            padding: 10px;
+            margin: 1px;
+            white-space: nowrap;
+        }
+        .dir-contents{
+            display: flex;
+            flex-direction: column;
+        }
+    </style>
 </head>
 <body>
-
+<div class="container">
+<div class="browse">
+<div style="width:100%;display:table;">
 <?php
-$indent = "----";
+$base_url = "http://localhost:9090/";
+$indent = "&nbsp;&nbsp;&nbsp;&nbsp;";
 function getDirContents($dir, &$tree) {
-    echo "$dir";
     $files = scandir($dir);
     foreach ($files as $key => $value) {
         $path = $dir . DIRECTORY_SEPARATOR . $value;
         if (!is_dir($path)) {
             if (pathinfo($value)['extension']=="html") {
-                $tree[] = $value;
+                $tree[] = $path;
             }
         } else if ($value[0] != ".") {
             $tree[$value] = array();
-            getDirContents($path, $tree[$value]);
+            $subtree = getDirContents($path, $tree[$value]);
+            if (count($subtree)==0) {
+                unset($tree[$value]);
+            }
         }
     }
     return $tree;
@@ -27,25 +76,52 @@ function printTree($tree, $depth=0){
     global $indent;
     foreach($tree as $key => $value) {
         if (is_array($value)){
-            echo str_repeat($indent, $depth) . "$key \n";
+            echo "<div class=\"dir\"><a class=\"dir-a entry\">" . str_repeat($indent, $depth) . "$key</a>\n";
+            echo "<div class=\"dir-contents\" style=\"display: none;\">\n";
             printTree($value, $depth+1);
+            echo "</div></div>\n";
         } else {
-            echo str_repeat($indent, $depth) . "$value\n";
+            $filename = pathinfo($value)['basename'];
+            $url = $base_url . substr($value, 2);
+            echo "<div class=\"file\"><a class=\"file-a entry\" name=\"$url\">" . str_repeat($indent, $depth) . "$filename</a></div>\n";
         }
     }
 }
 
 $tree = array();
-getDirContents('.', $tree);
+$tree = getDirContents('.', $tree);
 printTree($tree);
 
 ?>
-
-<script>
-    $(".childshow").click(function (e) {
-        e.stopPropagation();
-        jQuery(this).children('.childshow').toggle();
-    });
+    </div>
+</div>
+<div class="preview">
+    <div>
+    <iframe style="width:100%; height:100%;" id="preview"></iframe>
+    </div>
+</div>
+</div>
+<script type="text/javascript">
+    menus = function() {
+        var dirContent = this.parentElement.querySelector(".dir-contents");
+        console.log(dirContent);
+        if (dirContent.style.display === "none") {
+            dirContent.style.display = "block";
+        } else {
+            dirContent.style.display = "none";
+        }
+    };
+    preview = function() {
+        document.getElementById('preview').src = this.name;
+    }
+    var submenu = document.getElementsByClassName("dir-a");
+    for (var i = 0; i < submenu.length; i++) {
+        submenu[i].addEventListener('click', menus);
+    }
+    submenu = document.getElementsByClassName("file-a");
+    for (var i = 0; i < submenu.length; i++) {
+        submenu[i].addEventListener('click', preview);
+    }
 </script>
 </body>
-</htmll>
+</html>
